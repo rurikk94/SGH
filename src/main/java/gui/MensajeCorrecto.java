e95c2fcd2dbba1b3a0eabad09f29e5536ca53e8e
Sergio Mora <sergio94mora@gmail.com>
2018-07-23 08:43:44 -0400

    Se corrige error al cerrar mensajes de correcto/fallido.

--- src/main/java/gui/MensajeCorrecto.java
+++ src/main/java/gui/MensajeCorrecto.java
@@ -36,7 +36,7 @@ public class MensajeCorrecto extends JFrame {
 	 * Create the frame.
 	 */
 	public MensajeCorrecto() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
